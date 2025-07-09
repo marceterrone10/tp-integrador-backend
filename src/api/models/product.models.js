f08a8221b203f9f7e978c63a4afd9fc5b62f44b2
Marcelo Terrone <tatoterrone@gmail.com>
2025-07-09 14:17:53 -0300

    Modifique consulta SQL para que muestre prod activos solamente

--- src/api/models/product.models.js
+++ src/api/models/product.models.js
@@ -1,7 +1,7 @@
 import connection from '../database/db.js'; // Importamos la conexión a la base de datos
 
 export const selectAllProducts = async () => {
-    let sql = `SELECT * FROM productos;`; // hacemos la consulta sql para mostrar los prod
+    let sql = `SELECT * FROM productos WHERE activo = 1;`; // hacemos la consulta sql para mostrar los prod
     return await connection.query(sql); // desestructuramos la consulta para obtener filas - llamada a base de datos
 };
 
@@ -14,13 +14,15 @@ export const selectProductById = async (id) => {
 
 export const postProduct = async(nombre, marca, precio, categoria, activo, imagen) => {
     //consulta SQL para insertar un nuevo producto
+    const activoValue = Number(activo); //convierto el valor de activo a numero porque viene de un String
     let sql = `INSERT INTO productos (nombre, marca, precio, categoria, activo, imagen) VALUES (?, ?, ?, ?, ?, ?)`;
-    return await connection.query(sql, [nombre, marca, precio, categoria, activo, imagen]); // ejecutamos la consulta y desestructuramos para obtener las filas
+    return await connection.query(sql, [nombre, marca, precio, categoria, activoValue, imagen]); // ejecutamos la consulta y desestructuramos para obtener las filas
 };
 
 export const modifyProduct = async(nombre, marca, precio, categoria, activo, imagen, id) => {
+    const activoValue = Number(activo);
     let sql = `UPDATE productos SET nombre = ?, marca = ?, precio = ?, categoria = ?, activo = ?, imagen = ? WHERE id = ?`; // consulta SQL para actualizar el producto
-    return await connection.query(sql, [nombre, marca, precio, categoria, activo, imagen, id]); // ejecutamos la consulta y desestructuramos para obtener el resultado
+    return await connection.query(sql, [nombre, marca, precio, categoria, activoValue, imagen, id]); // ejecutamos la consulta y desestructuramos para obtener el resultado
 };
 
 export const deleteProductById = async(id) => {
